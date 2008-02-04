4f6ec2ff29be2e1fa2bff9e485aa0c4ca4dc0070
Adam Byrtek <adambyrtek@gmail.com>
2008-02-04 23:41:23 +0000

    Del.icio.us password removed from repository.

--- bin/backup-delicious.sh
+++ bin/backup-delicious.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # Backup del.icio.us items
+# first argument should be username, second a password
 
 DATE=$(date "+%Y%m%d")
 OUTPUT="$HOME/backup/delicious-backup-$DATE.xml"
 
-curl -s https://alpha.pl:harakiri@api.del.icio.us/v1/posts/all > $OUTPUT
+curl -s https://$1:$2@api.del.icio.us/v1/posts/all > $OUTPUT
