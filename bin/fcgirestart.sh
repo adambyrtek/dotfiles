3033045acc9c72c18dd3fe983a867982033b4399
Adam Byrtek <adambyrtek@gmail.com>
2008-03-04 16:32:40 +0000

    FastCGI script updated.

--- bin/fcgirestart.sh
+++ bin/fcgirestart.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-cd $1
-DISPATCH="$(pwd)/public/dispatch.fcgi"
-
-if [ ! -f $DISPATCH ]; then
-    echo "Dispatcher $DISPATCH doesn't exist"
+if [[ -z $1 ]]; then
+    echo "First argument should be a full path to the application"
     exit 1
 fi
 
+DISPATCH="$1/public/dispatch.fcgi"
 PIDS=$(pgrep -f $DISPATCH)
+
+if [[ -z $PIDS ]]; then
+    echo "No processes for dispatcher $DISPATCH found"
+    echo "Are you sure you've provided the full path?"
+    exit 1
+fi
+
 echo "Killing the following processes: $PIDS"
 kill $PIDS
