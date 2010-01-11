06d46dd6e27c1d4c14ca04b02b9f625d34228740
Adam Byrtek <adambyrtek@google.com>
2010-01-11 02:09:44 +0100

    xmonad configuration added.

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,13 @@
+import XMonad
+import XMonad.Config.Gnome
+import XMonad.ManageHook
+import XMonad.Util.EZConfig
+
+myManageHook :: [ManageHook]
+myManageHook =
+    [ resource =? "Do"   --> doIgnore ]
+
+main = xmonad $ gnomeConfig
+    { modMask = mod4Mask, manageHook = manageHook gnomeConfig <+> composeAll myManageHook }
+    `additionalKeysP`
+        [ ("M-S-l",    spawn "gnome-screensaver-command -l") ]
