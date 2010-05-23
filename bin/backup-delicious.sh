1bd4ef56ceedc1b62a60b404f2ff03f5a36b0199
Adam Byrtek <adambyrtek@gmail.com>
2010-05-23 21:15:36 +0200

    bin directory removed.

--- bin/backup-delicious.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Backup del.icio.us items
-# first argument should be username, second a password
-
-DATE=$(date "+%Y%m%d")
-OUTPUT="$HOME/backup/delicious-backup-$DATE.xml"
-
-curl -s https://$1:$2@api.del.icio.us/v1/posts/all > $OUTPUT
