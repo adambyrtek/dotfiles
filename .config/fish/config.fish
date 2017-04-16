bc44e6e087418c4b89aa6a1e86191f7e9a2c3ea8
Adam Byrtek <adambyrtek@gmail.com>
2017-04-16 20:13:38 +0000

    Git abbreviations for Fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,17 @@
 set -x EDITOR vim
+
+abbr -a ga git add
+abbr -a gb git branch -v
+abbr -a gc git commit -v
+abbr -a gco git checkout
+abbr -a gcom git checkout master
+abbr -a gd git diff
+abbr -a gf git fetch -p
+abbr -a gh git help
+abbr -a gm git merge
+abbr -a gp git push
+abbr -a gl git pull -p
+abbr -a glg git log --graph
+abbr -a gr git remote -v
+abbr -a gst git status -sb
+abbr -a gt git tag
