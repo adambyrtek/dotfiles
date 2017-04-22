c58c9d1374414b676aebaa9483c84dfa5b449d03
Adam Byrtek <adambyrtek@gmail.com>
2017-04-22 15:55:26 +0000

    Add IPython configuration

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,17 @@
+# Colors for tracebacks and object info (linux is brighter)
+c.TerminalInteractiveShell.colors = 'linux'
+
+# Pygments syntax highlighting style
+c.TerminalInteractiveShell.highlighting_style = 'monokai'
+
+# Show completions in a single column with type on the right
+c.TerminalInteractiveShell.display_completions = 'column'
+
+# Leave room for more completions
+c.TerminalInteractiveShell.space_for_menu = 10
+
+# Require magic functions to be called explicitly with %
+c.InteractiveShell.automagic = False
+
+# Invoke external editor using Ctrl-X Ctrl-E
+c.TerminalInteractiveShell.extra_open_editor_shortcuts = True 
